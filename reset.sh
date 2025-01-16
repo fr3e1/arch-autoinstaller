0443d8685fec07f2d35fded96ef658bb070fb8cc
fr3e1 <fr3e.acc0unt@gmail.com>
2025-01-16 09:37:15 -0700

    Added disclaimer

--- reset.sh
+++ reset.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-umount --lazy /mnt/boot
-umount --lazy /mnt
-echo "done"
+echo "MAINLY USED FOR TESTING PURPOSES AND IS NOT MEANT TO BE USED FOR USERS"
+read -p "continue? [y/N]" continue
+case "$continue" in 
+	[yY][eE][sS]|[yY])
+		umount --lazy /mnt/boot
+		umount --lazy /mnt/boot
+		echo "done"
+		;;
+	*)
+		echo "Exiting..."
+		exit 1
+		;;
+esac
+
