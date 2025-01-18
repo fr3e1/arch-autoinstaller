1601f2fc741e9112225ff92c699a11f58be3614d
fr3e1 <fr3e.acc0unt@gmail.com>
2025-01-17 21:36:40 -0700

    big update

---
+++ preset.sh
@@ -0,0 +1,51 @@
+# 💫 https://github.com/JaKooLit 💫 #
+#
+# CREDIT to: https://github.com/PostCyberPunk for this Preset function
+
+#CUSTOMIZED BY FR3E
+#CUSTOMIZED BY FR3E
+#CUSTOMIZED BY FR3E
+
+#CUSTOMIZED BY FR3E
+#CUSTOMIZED BY FR3E
+#CUSTOMIZED BY FR3E
+
+
+aur_helper="yay"
+
+nvidia="Y"
+
+gtk_themes="Y"
+###-Do you want to configure Bluetooth?
+bluetooth="Y"
+###-Do you want to install Thunar file manager?
+thunar="Y"
+###-Install & configure SDDM log-in Manager plus (OPTIONAL) SDDM Theme?
+sddm="N"
+###-Install XDG-DESKTOP-PORTAL-HYPRLAND? (For proper Screen Share ie OBS)
+xdph="Y"
+###-Install zsh, oh-my-zsh & (Optional) pokemon-colorscripts?
+zsh="N"
+###-Installing in a Asus ROG Laptops?
+rog="N"
+###-Do you want to download pre-configured Hyprland dotfiles?
+dots="Y"
+
+### These are the sub-questions of the above choices
+### Would you like to blacklist nouveau? (y/n)
+blacklist_nouveau="Y"
+# Ask the user if they want to use Thunar as the default file manager
+# Do you want to set Thunar as the default file manager? (y/n): "
+thunar_default="Y"
+### XDG-desktop-portal-KDE & GNOME (if installed) should be manually disabled or removed! Script cant remove nor disable it.
+### Would you like to try to remove other XDG-Desktop-Portal-Implementations? (y/n) 
+XDPH1="Y"
+### OPTIONAL - Would you like to install SDDM themes? (y/n)
+install_sddm_theme=""
+### " This script will add your user to the 'input' group."
+### " Please note that adding yourself to the 'input' group might be necessary for waybar keyboard-state functionality."
+input_group_choid="Y"
+### OPTIONAL - Do you want to add Pokemon color scripts? (y/n): 
+pokemon_choice="N"
+### Do you want to upgrade to the latest version? (y/n) - This is for the dotfiles
+upgrade_choice="Y"
--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# replacing pacman conf
+
+rm /etc/pacman.conf
+cp pacman.conf /etc/pacman.conf
+
 # User input for drive, username, password, and timezone
 read -p "Drive to install Arch on (e.g., sda, nvme0n1): " drive
 read -p "Hostname: " hostname
@@ -13,15 +18,20 @@ read -p "Username: " username
 read -sp "Password: " password
 echo
 read -sp "Retype Password: " validating
-echo
 read -p "Timezone (Region/City): " timezone
+read -p "Install vidia drivers (y/N): " nvidia
 
 # Verify passwords match
 if [[ "$password" != "$validating" ]]; then
     echo "Passwords do not match. Exiting..."
     exit 1
 fi
+# Editing preset for nvidia driver
 
+case "$nvidia" in 
+	[yY][eE][sS]|[yY]) 
+
+		sed -i '16s/.*/nvidia="Y"/' preset.sh
 # Validate the drive
 if [[ ! -b "/dev/$drive" ]]; then
     echo "Invalid drive specified. Exiting..."
@@ -63,10 +73,12 @@ mkdir /mnt/boot
 mount "/dev/${drive}1" /mnt/boot || { echo "Failed to mount /dev/${drive}1"; exit 1; }
 
 # Install the base system
-pacstrap /mnt base linux linux-firmware base-devel efibootmgr grub networkmanager zsh git sudo gdm hyprland || {
+pacstrap /mnt base linux linux-firmware base-devel efibootmgr grub networkmanager zsh git sudo gdm hyprland kitty firefox || {
     echo "Failed to install base system. Exiting..."
     exit 1
 }
+
+# Generating fstab
 
 genfstab /mnt > /mnt/etc/fstab
 
@@ -86,7 +98,7 @@ locale-gen
 useradd -m -G wheel -s /bin/bash "$username"
 echo "$username:$password" | chpasswd
 
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
 
 # Install and configure GRUB
 grub-install --efi-directory=/boot
@@ -97,9 +109,35 @@ fi
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# Installing dots
+
+su - $username -c "./Arch-Hyprland/install.sh
+
 # Enable essential services
 systemctl enable NetworkManager
 systemctl enable gdm
+EOF
+
+
+# Post-install setup
+
+git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git /mnt/home/$username/Arch-Hyprland
+
+git clone https://github.com/sejjy/mechabar.git /mnt/home/$username/mechabar
+
+rm /mnt/home/$username/Arch-hyprland/preset.sh
+cp preset.sh /mnt/home/$username/Arch-Hyprland/preset.sh
+
+arch-chroot /mnt /bin/bash <<EOF
+pacman -Sy bluez-utils brightnessctl hyprlock pipewire pipewire-pulse python ttf-jetbrains-mono-nerd wireplumber --noconfirm
+
+su - $username -c "yes | sh ~/Arch-Hyprland/install.sh"
+su - $username -c "yay -S bluetui rofi-lbonn-wayland-git"
+su - $username -c "sh ~/mechabar/install.sh'
+
+
+sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
 EOF
 
 # Final unmount for main partitions
